82a17e319693b96f5823c35b0073057d092bf2cb
Krzysztof Kin <sirex0@gmail.com>
2018-04-10 21:44:41 +0200

    initial commit

---
+++ app/src/main/java/cloud/krzysztofkin/fragmentjuggler/FragmentPageAdapter.java
@@ -0,0 +1,34 @@
+package cloud.krzysztofkin.fragmentjuggler;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+class FragmentPageAdapter extends FragmentPagerAdapter {
+    FragmentPageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    /**
+     * Return the Fragment associated with a specified position.
+     *
+     * @param position position
+     */
+    @Override
+    public Fragment getItem(int position) {
+        return ParametricFragment.newInstance("Parameter: " + Integer.toString(position));
+    }
+
+    /**
+     * Return the number of views available.
+     */
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "POSITION " + Integer.toString(position);
+    }
+}
---
+++ app/src/main/java/cloud/krzysztofkin/fragmentjuggler/ViewPagerFragment.java
@@ -0,0 +1,47 @@
+package cloud.krzysztofkin.fragmentjuggler;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ViewPagerFragment extends Fragment {
+
+    ViewPager viewPager;
+    TabLayout tabLayout;
+
+    public ViewPagerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_view_pager, container, false);
+
+        //Create viewPager to swipe between categories
+        viewPager = rootView.findViewById(R.id.view_pager);
+        // Create an adapter that knows which fragment should be shown on each page
+        FragmentPageAdapter adapter = new FragmentPageAdapter(getFragmentManager());
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+
+        //TabLayout
+        tabLayout =  rootView.findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return rootView;
+    }
+
+}
